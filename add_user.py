f0678953ae3909fdf933de86be78185279c6d818
Dũng Nguyễn Hoàng <dungdore1312@gmail.com>
2022-04-19 19:59:23 +0700

    Minor fix on relative path and static folder

--- add_user.py
+++ add_user.py
@@ -1,9 +1,11 @@
 from src.app import db, user_datastore
 
 db.create_all()
+email = input('Enter User Email: ')
+password = input('Enter Password: ')
 user = user_datastore.create_user(
-    email='admin@test.com',
-    password='admin'
+    email=email,
+    password=password
 )
 db.session.add(user)
 db.session.commit()
